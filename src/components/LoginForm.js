017e8eeca400f3aaef0b13247545a2206394aa12
Andrii Mutylo <amutylo@gmail.com>
2017-12-09 20:36:33 +0200

    firebase auth

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import {Text} from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input} from './common';
 
 class LoginForm extends Component{
-  state = {email: ''};
+  state = {email: '', password: '', error: ''};
+
+  onButtonPress(){  
+    const { email, password } = this.state;
   
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch(() => {
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch(() => {
+        this.setState({error: 'Authentication fail.'})
+      })
+    })
+  }
+
   render(){
     return(
       <Card>
@@ -11,14 +25,24 @@ class LoginForm extends Component{
           <Input 
             placeholder='user@email.com'
             label='Email'
+            value={this.state.password}
+            onChangeText={password => this.setState({password})} 
+          />
+        </CardSection>
+        <CardSection>
+          <Input 
+            secureTextEntry
+            placeholder='password'
+            label='Password'
             value={this.state.email}
             onChangeText={email => this.setState({email})} 
           />
         </CardSection>
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
         <CardSection>
-        </CardSection>
-        <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
           Log in
           </Button>
         </CardSection>
@@ -27,4 +51,11 @@ class LoginForm extends Component{
   }
 }
 
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+}
 export default LoginForm;
