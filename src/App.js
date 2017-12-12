50688de1992b116d79af2784136dba95b4eecffd
Andrii Mutylo <amutylo@gmail.com>
2017-12-12 21:54:02 +0200

    added Log out action to button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
+  state = { loggedIn: null };
+  
   componentWillMount(){
+
     firebase.initializeApp({
       apiKey: 'AIzaSyAKjN4WhJ-Qz-cqzZ9UDU1XpW5WD0Ai7fI',
       authDomain: 'auth-269d0.firebaseapp.com',
@@ -16,12 +19,39 @@ class App extends Component {
       messagingSenderId: '478315114905'
     });
   
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+});
   }
+
+  renderContent(){
+    switch(this.state.loggedIn) {
+      case false:
+      return <LoginForm />
+
+      case true:
+      return(
+        <CardSection>
+          <Button onPress={() => {firebase.auth().signOut()}}>
+            Log Out
+          </Button>
+        </CardSection>
+        );
+      
+      default:
+        return <Spinner size="large" />
+    }
+  }
+
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
